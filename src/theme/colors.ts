cf33543e76f6f1b5c6f71facf2c00b3309ec184d
leferzzz <97904761+leferzzz@users.noreply.github.com>
2022-01-27 14:12:02 -0400

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -6,9 +6,9 @@ export const green = {
 };
 
 export const orange = {
-  200: '#bd7cfe',
-  400: '#ab47bc',
-  700: '#7b1fa2',
+  200: '#F95B3D',
+  400: '#F95B3D',
+  700: '#F95B3D',
 };
 
 export const red = {
