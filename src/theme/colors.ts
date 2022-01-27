762b05d93671ac14305107c513d75eb51874946f
leferzzz <97904761+leferzzz@users.noreply.github.com>
2022-01-27 14:05:20 -0400

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -18,9 +18,9 @@ export const red = {
 };
 
 export const grey = {
-  100: '#F95B3D',
+  100: '#f5f5f5',
   200: '#eeeeee',
-  300: '#F95B3D',
+  300: '#e0e0e0',
   400: '#bdbdbd',
   500: '#9e9e9e',
   600: '#757575',
